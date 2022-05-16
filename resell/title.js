34a3c26e62a5488bf86b45e3cb2da922417df583
fault <99757068+prvtfault@users.noreply.github.com>
2022-05-16 19:49:29 +0200

    Add files via upload

---
+++ resell/title.js
@@ -0,0 +1,3 @@
+if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
+function loaded(){setInterval(loop,800);}
+var x=0;var titleText=["fault#1337","resell","page", "imagine","not","using","fault's","service", ":D","fault.cash",];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
