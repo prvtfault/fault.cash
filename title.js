e91b91204d74b34d251840a27cbbc718c4bbbc84
fault <99757068+prvtfault@users.noreply.github.com>
2022-05-21 23:17:50 +0200

    Add files via upload

--- title.js
+++ title.js
@@ -1,4 +1,3 @@
 if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
 function loaded(){setInterval(loop,800);}
-var x=0;var titleText=["fault#1337","imagine","not", "cheating", ":D","fault.cash",];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
-
+var x=0;var titleText=["fault#1337","resell","page", "imagine","not","using","fault's","service", ":D","fault.cash",];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
